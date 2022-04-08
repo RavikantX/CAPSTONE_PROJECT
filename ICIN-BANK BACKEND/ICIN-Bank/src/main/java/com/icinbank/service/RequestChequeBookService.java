14685261ad2fe5e035bfc6a581191676994a8bd2
RavikantX <ravikant97087@gmail.com>
2022-04-08 16:08:54 +0530

    Final Project

---
+++ ICIN-BANK BACKEND/ICIN-Bank/src/main/java/com/icinbank/service/RequestChequeBookService.java
@@ -0,0 +1,16 @@
+package com.icinbank.service;
+
+
+import java.util.List;
+
+import com.icinbank.domain.RequestChequeBook;
+
+public interface RequestChequeBookService {
+	RequestChequeBook createRequestChequeBook(RequestChequeBook requestChequeBook);
+
+    List<RequestChequeBook> findAll();
+
+    RequestChequeBook findRequestChequeBook(Long id);
+
+    void confirmRequestChequeBook(Long id);
+}
